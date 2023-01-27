f701cc9087920dc8b5f323d4a7118113744d97ac
MayconnMancini <mayconnkayto@hotmail.com>
2023-01-27 16:11:50 -0400

    melhoriass

--- src/routes/jogo.ts
+++ src/routes/jogo.ts
@@ -515,7 +515,7 @@ export async function jogoRoutes(fastify: FastifyInstance) {
 
       jogosBD.map((item) => {
         resp.map(async (r: any) => {
-          if (item.fixtureIdApi === r.fixture.id && r.fixture.status.losg === "Match Finished") {
+          if (item.fixtureIdApi === r.fixture.id && r.fixture.status.long === "Match Finished") {
             console.log("id encontrado")
             console.log(r.fixture.id)
             await prisma.jogo.update({
