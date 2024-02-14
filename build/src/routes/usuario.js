1b23972b4b73ba7d2da2e6952c876a0dec5f0fa2
MayconnMancini <mayconnkayto@hotmail.com>
2024-02-14 19:09:17 -0400

    changs

--- build/src/routes/usuario.js
+++ build/src/routes/usuario.js
@@ -5,11 +5,11 @@ const prisma_1 = require("../lib/prisma");
 const zod_1 = require("zod");
 const authenticate_1 = require("../plugins/authenticate");
 async function usuarioRoutes(fastify) {
-    fastify.get('/usuarios/count', async () => {
+    fastify.get("/usuarios/count", async () => {
         const count = await prisma_1.prisma.usuario.count();
         return { count };
     });
-    fastify.post('/updateUserInfo', { onRequest: [authenticate_1.authenticate] }, async (request, reply) => {
+    fastify.post("/updateUserInfo", { onRequest: [authenticate_1.authenticate] }, async (request, reply) => {
         try {
             const signupBody = zod_1.z.object({
                 nome: zod_1.z.string(),
@@ -21,41 +21,42 @@ async function usuarioRoutes(fastify) {
             const { nome, email, cidade, estado, telefone } = signupBody.parse(request.body);
             let usuario = await prisma_1.prisma.usuario.findFirst({
                 where: {
-                    email: email
-                }
+                    email: email,
+                },
             });
             if (!usuario) {
                 return reply.status(400).send({
-                    message: 'Email não cadastrado'
+                    message: "Email não cadastrado",
                 });
             }
-            if (usuario.id != parseInt(request.user.sub) && usuario.isAdmin == false) {
+            if (usuario.id != parseInt(request.user.sub) &&
+                usuario.isAdmin == false) {
                 return reply.status(400).send({
-                    message: 'Sem permissão para atualizar informações'
+                    message: "Sem permissão para atualizar informações",
                 });
             }
             await prisma_1.prisma.usuario.update({
                 where: {
-                    email: email
+                    email: email,
                 },
                 data: {
                     nome: nome,
                     cidade: cidade,
                     estado: estado,
-                    telefone: telefone
-                }
+                    telefone: telefone,
+                },
             });
             return reply.status(201).send({
-                message: "Informações do atualizadas com sucesso"
+                message: "Informações do atualizadas com sucesso",
             });
         }
         catch (error) {
             return reply.status(500).send({
-                message: `Erro interno do servidor -> ${error}`
+                message: `Erro interno do servidor -> ${error}`,
             });
         }
     });
-    fastify.get('/getUserInfo/:email', { onRequest: [authenticate_1.authenticate] }, async (request, reply) => {
+    fastify.get("/getUserInfo/:email", { onRequest: [authenticate_1.authenticate] }, async (request, reply) => {
         try {
             const signupBody = zod_1.z.object({
                 email: zod_1.z.string().email(),
@@ -63,22 +64,23 @@ async function usuarioRoutes(fastify) {
             const { email } = signupBody.parse(request.params);
             let usuario = await prisma_1.prisma.usuario.findFirst({
                 where: {
-                    email: email
-                }
+                    email: email,
+                },
             });
             if (!usuario) {
                 return reply.status(400).send({
-                    message: 'Email não cadastrado'
+                    message: "Email não cadastrado",
                 });
             }
-            if (usuario.id != parseInt(request.user.sub) && request.user.isAdmin === false) {
+            if (usuario.id != parseInt(request.user.sub) &&
+                request.user.isAdmin === false) {
                 return reply.status(400).send({
-                    message: 'Sem permissão para vizualizar informações'
+                    message: "Sem permissão para vizualizar informações",
                 });
             }
             return await prisma_1.prisma.usuario.findFirst({
                 where: {
-                    email: email
+                    email: email,
                 },
                 select: {
                     id: true,
@@ -91,12 +93,27 @@ async function usuarioRoutes(fastify) {
                     avatarUrl: true,
                     isAdmin: true,
                     createdAt: true,
-                }
+                },
             });
         }
         catch (error) {
             return reply.status(500).send({
-                message: `Erro interno do servidor -> ${error}`
+                message: `Erro interno do servidor -> ${error}`,
+            });
+        }
+    });
+    fastify.get("/getAllUsers", { onRequest: [authenticate_1.authenticate] }, async (request, reply) => {
+        try {
+            if (request.user.isAdmin === false) {
+                return reply.status(400).send({
+                    message: "Sem permissão para vizualizar informações",
+                });
+            }
+            return await prisma_1.prisma.usuario.findMany();
+        }
+        catch (error) {
+            return reply.status(500).send({
+                message: `Erro interno do servidor -> ${error}`,
             });
         }
     });
